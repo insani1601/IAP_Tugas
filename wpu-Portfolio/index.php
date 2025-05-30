d5b9eb62dae0f031e557d554a8ea2da7a78a623a
insani1601 <iverzama145@gmail.com>
2025-05-31 01:33:08 +0700

    mengabungka data api youtube dan menghubungkan dengan web

--- wpu-Portfolio/index.php
+++ wpu-Portfolio/index.php
@@ -1,16 +1,33 @@
+<?php
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, 'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCBO1vtJfJQrxzdEMaSyNfSQ&key=AIzaSyDeBTJsTcQFnBi4fcN6g2bz4FRiDJCk0bM');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($curl);
+curl_close($curl);
+
+$data = json_decode($result, true);
+
+if (!isset($data['items'][0])) {
+    $title = "Channel Not Found";
+    $subscribers = 0;
+    $thumbnail = "img/profile2.png";
+} else {
+    $channel = $data['items'][0];
+    $title = $channel['snippet']['title'];
+    $subscribers = $channel['statistics']['subscriberCount'];
+    $thumbnail = $channel['snippet']['thumbnails']['medium']['url'];
+}
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
-    <!-- My CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-
     <title>My Portfolio</title>
   </head>
   <body>
@@ -18,195 +35,124 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
         <a class="navbar-brand" href="#home">Sandhika Galih</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#portfolio">Portfolio</a>
-            </li>
+            <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
           </ul>
         </div>
       </div>
     </nav>
 
-
     <div class="jumbotron" id="home">
-      <div class="container">
-        <div class="text-center">
-          <img src="img/profile1.png" class="rounded-circle img-thumbnail">
-          <h1 class="display-4">Sandhika Galih</h1>
-          <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
-        </div>
+      <div class="container text-center">
+        <img src="img/profile1.png" class="rounded-circle img-thumbnail">
+        <h1 class="display-4">Sandhika Galih</h1>
+        <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
       </div>
     </div>
 
-
-    <!-- About -->
     <section class="about" id="about">
       <div class="container">
-        <div class="row mb-4">
-          <div class="col text-center">
-            <h2>About</h2>
-          </div>
+        <div class="row mb-4 text-center">
+          <div class="col"><h2>About</h2></div>
         </div>
         <div class="row justify-content-center">
           <div class="col-md-5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, molestiae sunt doloribus error ullam expedita cumque blanditiis quas vero, qui, consectetur modi possimus. Consequuntur optio ad quae possimus, debitis earum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
           </div>
           <div class="col-md-5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, molestiae sunt doloribus error ullam expedita cumque blanditiis quas vero, qui, consectetur modi possimus. Consequuntur optio ad quae possimus, debitis earum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
           </div>
         </div>
       </div>
     </section>
 
-
-   <!-- Youtube & IG -->
     <section class="social bg-light" id="social"> 
-  <div class="container">
-    <div class="row pt-4 mb-4">
-      <div class="col text-center">
-        <h2>Social Media</h2>
-      </div>
-    </div>
+      <div class="container">
+        <div class="row pt-4 mb-4 text-center">
+          <div class="col"><h2>Social Media</h2></div>
+        </div>
 
-<div class="row justify-content-center">
-  <div class="col-md-5"> 
-    <div class="row">
-      <div class="col md-4">
-        <img src="img/profile2.png" width="200" class="rounded-circle img-thumbnail">
-      </div>
-      <div class="col md-8">
-        <h5>insani_vzm</h5>  
-        <p>100 Subscribers.</p>
-      </div> 
-    </div>
-   <div class="row mt- pb-3 ">
-    <div class="col">
-      <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/NKo7vBX9zPQ?rel=0" allowfullscreen></iframe>
-      </div>
-    </div>
-   </div> 
-  </div>
-  <div class="col-md-5">
-  <div class="row align-items-center">
-    <div class="col-md-4">
-      <img src="img/profile2.png" width="200" class="rounded-circle img-thumbnail">
-    </div>
-    <div class="col-md-8">
-      <h5>insani_vzm</h5>  
-      <p>100 Followers.</p>
-    </div>
-  </div>
+        <div class="row justify-content-center">
+          <div class="col-md-5"> 
+            <div class="row">
+              <div class="col-md-4">
+                <img src="<?= $thumbnail ?>" width="200" class="rounded-circle img-thumbnail">
+              </div>
+              <div class="col-md-8">
+                <h5><?= $title ?></h5>  
+                <p><?= number_format($subscribers) ?> Subscribers.</p>
+              </div> 
+            </div>
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/NKo7vBX9zPQ?rel=0" allowfullscreen></iframe>
+                </div>
+              </div>
+            </div> 
+          </div>
 
- <div class="row mt-3 pb-3">
-        <div class="col">
-         <div class="ig-thumbnail">
-          <img src="img/thumbs/1.png">
+          <div class="col-md-5">
+            <div class="row align-items-center">
+              <div class="col-md-4">
+                <img src="img/profile2.png" width="200" class="rounded-circle img-thumbnail">
+              </div>
+              <div class="col-md-8">
+                <h5>insani_vzm</h5>  
+                <p>100 Followers.</p>
+              </div>
+            </div>
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/1.png">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
- </div>
-</div>
- </section>
+    </section>
 
-
-    <!-- Portfolio -->
     <section class="portfolio" id="portfolio">
       <div class="container">
-        <div class="row pt-4 mb-4">
-          <div class="col text-center">
-            <h2>Portfolio</h2>
-          </div>
+        <div class="row pt-4 mb-4 text-center">
+          <div class="col"><h2>Portfolio</h2></div>
         </div>
         <div class="row">
+          <?php for ($i = 1; $i <= 6; $i++): ?>
           <div class="col-md mb-4">
             <div class="card">
-              <img class="card-img-top" src="img/thumbs/1.png" alt="Card image cap">
+              <img class="card-img-top" src="img/thumbs/<?= $i ?>.png" alt="Card image cap">
               <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
             </div>
           </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/3.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>   
-        </div>
-
-        <div class="row">
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/4.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div> 
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/5.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md mb-4">
-            <div class="card">
-              <img class="card-img-top" src="img/thumbs/6.png" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-          </div>
+          <?php endfor; ?>
         </div>
       </div>
     </section>
 
-
-    <!-- Contact -->
     <section class="contact bg-light" id="contact">
       <div class="container">
-        <div class="row pt-4 mb-4">
-          <div class="col text-center">
-            <h2>Contact</h2>
-          </div>
+        <div class="row pt-4 mb-4 text-center">
+          <div class="col"><h2>Contact</h2></div>
         </div>
-
         <div class="row justify-content-center">
           <div class="col-lg-4">
             <div class="card bg-primary text-white mb-4 text-center">
               <div class="card-body">
                 <h5 class="card-title">Contact Me</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Feel free to reach out via the form or social media!</p>
               </div>
             </div>
-            
             <ul class="list-group mb-4">
               <li class="list-group-item"><h3>Location</h3></li>
               <li class="list-group-item">My Office</li>
@@ -214,9 +160,7 @@
               <li class="list-group-item">West Java, Indonesia</li>
             </ul>
           </div>
-
           <div class="col-lg-6">
-            
             <form>
               <div class="form-group">
                 <label for="nama">Nama</label>
@@ -234,38 +178,21 @@
                 <label for="message">Message</label>
                 <textarea class="form-control" id="message" rows="3"></textarea>
               </div>
-              <div class="form-group">
-                <button type="button" class="btn btn-primary">Send Message</button>
-              </div>
+              <button type="submit" class="btn btn-primary">Send Message</button>
             </form>
-
           </div>
         </div>
       </div>
     </section>
 
-
-    <!-- footer -->
     <footer class="bg-dark text-white mt-5">
-      <div class="container">
-        <div class="row">
-          <div class="col text-center">
-            <p>Copyright &copy; 2018.</p>
-          </div>
-        </div>
+      <div class="container text-center">
+        <p>&copy; <?= date("Y") ?>. All rights reserved.</p>
       </div>
     </footer>
 
-
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
   </body>
 </html>
